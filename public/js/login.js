0b0d5372e541e4c8fd2bc7f7859ee27f0311e40f
Michael Hobert <michaelhobertjr315@gmail.com>
2023-04-18 11:37:27 -0400

    finished login.js and logout.js

--- public/js/login.js
+++ public/js/login.js
@@ -26,13 +26,13 @@ const loginFormHandler = async (event) => {
     event.preventDefault();
   
     const name = document.querySelector('#name-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
+    const username = document.querySelector('#username-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
   
-    if (name && email && password) {
+    if (name && username && password) {
       const response = await fetch('/api/users', {
         method: 'POST',
-        body: JSON.stringify({ name, email, password }),
+        body: JSON.stringify({ name, username, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
